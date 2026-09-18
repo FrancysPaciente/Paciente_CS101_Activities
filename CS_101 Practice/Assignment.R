5c46981d9adfa3a823b7d8e9ec9e28a786c25ef6
FrancysPaciente <francyschriel.paciente@students.isatu.edu.ph>
2026-09-18 00:05:29 +0000

    Assignment Submission

---
+++ CS_101 Practice/Assignment.R
@@ -0,0 +1,7 @@
+print("Assignment")
+
+print("Explain the difference between git add and git commit in your own words. Why are both steps necessary?")
+print("Answer: Git add tells the repository will be included in the next version. Git commit is a checkpoint to track down what changed in the next following versions. They are necessary because git add lets you select what goes with a commit while git commit records an update processed by the user.")
+
+print("A classmate says: If I delete a committed file from Github, my local copy will also be deleted. Is this correct? What command would update the local copy?")
+print("Answer: No, it will not be deleted immediately. A git pull will be used first to update the local for the modification to apply.")
